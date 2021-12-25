c6e935f7f399c63ba895390410dcefe83ffebdae
binodPP <binod.singh1503@gmail.com>
2021-12-25 22:46:15 +0530

    solved leetcode problem - 11,13

---
+++ src/com/binod/basicdatastructureandalgorithmoperation/array/RegularExpressionMatching.java
@@ -0,0 +1,24 @@
+package com.binod.basicdatastructureandalgorithmoperation.array;
+
+public class RegularExpressionMatching {
+   //https://leetcode.com/problems/regular-expression-matching/
+    public static void main(String[] args) {
+        String s = "aa";
+        String pattern = "a";
+        System.out.println(isMatch(s,pattern));
+    }
+
+
+    public static boolean isMatch(String text, String pattern) {
+        if (pattern.isEmpty()) return text.isEmpty();
+        boolean first_match = (!text.isEmpty() &&
+                (pattern.charAt(0) == text.charAt(0) || pattern.charAt(0) == '.'));
+
+        if (pattern.length() >= 2 && pattern.charAt(1) == '*'){
+            return (isMatch(text, pattern.substring(2)) ||
+                    (first_match && isMatch(text.substring(1), pattern)));
+        } else {
+            return first_match && isMatch(text.substring(1), pattern.substring(1));
+        }
+    }
+}
